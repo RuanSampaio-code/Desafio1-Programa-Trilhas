1a37b76459e71ca730afd98b9962c828805aeac3
RuanSampaio-code <ruansampaio001@gmail.com>
2025-01-30 23:37:15 -0300

    questões inicias do desafio

---
+++ q1.js
@@ -0,0 +1,2 @@
+let nome = "José Ruan";
+console.log(nome); 
---
+++ q2.js
@@ -0,0 +1,3 @@
+let idade = 25;
+let altura = 1.75;
+console.log("idade de " + idade, "e altura de", altura); // Exibe os valores de idade e altura
